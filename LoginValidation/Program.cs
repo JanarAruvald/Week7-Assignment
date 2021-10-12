cbbd815318c15ead8a776666f8fd25a53aa4301a
Opilane TTHK <opilane@tthk.ee>
2021-10-12 11:31:42 +0300

    WeekSevenAssignment

---
+++ LoginValidation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            
+            int i = 0;
+            int counter = 0;
+
+            while (i < 3)
+            {
+                string Login;
+                string Password;
+                Console.WriteLine("Login: ");
+                Login = Console.ReadLine();
+                Console.WriteLine("Password: ");
+                Password = Console.ReadLine();
+                Console.WriteLine("PIN-kood: ");
+                string PINcode = Console.ReadLine();
+
+
+                if (PINcode == "1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                
+                else if ((Login == "admin") && (Password == "pass1234") && (PINcode == "1234"))
+                {
+                    Console.WriteLine("Tere tulemast!");
+                }
+                else if ((Login != "admin") && (Password == "pass1234"))
+                {
+                    Console.WriteLine("Midagi läks valesti. Proovi uuesti.");
+                }
+                else if ((Login == "admin") && (Password != "pass1234"))
+                {
+                    Console.WriteLine("Midagi läks valesti. Proovi uuesti.");
+                }
+                else
+                {
+                    i++; //i = i + 1
+                    Console.WriteLine($"Proovi uuesti! {3 - i} katset on jäänud.");
+                }
+
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ RandomLetter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RandomLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string name = Console.ReadLine();
+
+            int counter = 0;
+
+            foreach (char character in name)
+            {
+                 counter++;
+            }
+
+            Console.WriteLine($"Nimi {name} on {counter} sümbolit pikk.");
+
+
+
+
+
+        }
+    }
+}
